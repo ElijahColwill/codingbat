796722b436ca0f566bb6c827adff69889d07fc7a
Elijah Robert Colwill <colwiller@dcsdk12.org>
2016-09-19 12:22:36 -0600

    Implement make_out_word function

--- codingbat.py
+++ codingbat.py
@@ -16,3 +16,11 @@ def make_tags(tag, word):
 
 print "--"
 print make_tags("i", "Yay")
+
+def make_out_word(out, word):
+    return out[:2] + word + out[2:]
+
+print "--"
+print make_out_word('<<>>', 'Yay') # '<<Yay>>
+print make_out_word('<<>>', 'WooHoo') # '<<WooHoo>>'
+print make_out_word('[[]]', 'word') # '[[word]]'
