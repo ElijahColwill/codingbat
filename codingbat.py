0c84485d3bf349128a46209ffc19d4c8db4df8f0
Elijah Robert Colwill <colwiller@dcsdk12.org>
2016-09-21 11:56:08 -0600

    Implement first_last6

--- codingbat.py
+++ codingbat.py
@@ -166,3 +166,17 @@ print "--"
 print xyz_there('abcxyz') # True
 print xyz_there('abc.xyz') # False
 print xyz_there('xyz.abc') # True
+
+# LIST 1
+
+def first_last6(nums):
+    if nums[0] == 6:
+        return True
+    if nums[-1] == 6:
+        return True
+    else: return False
+
+print "--"
+print first_last6([1, 2, 6]) # True
+print first_last6([6, 1, 2, 3]) # True
+print first_last6([13, 6, 1, 2, 3]) # False
