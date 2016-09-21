cf022b1d0635411f2ceeb498c992cfd62de683c1
Elijah Robert Colwill <colwiller@dcsdk12.org>
2016-09-21 12:42:51 -0600

    Implement big_diff function

--- codingbat.py
+++ codingbat.py
@@ -299,3 +299,11 @@ print "--"
 print count_evens([2, 1, 2, 3, 4]) # 3
 print count_evens([2, 2, 0]) # 3
 print count_evens([1, 3, 5]) # 0
+
+def big_diff(nums):
+    return max(nums) - min(nums)
+
+print "--"
+print big_diff([10, 3, 5, 6]) # 7
+print big_diff([7, 2, 10, 9]) # 8
+print big_diff([2, 10, 7, 2]) # 8
