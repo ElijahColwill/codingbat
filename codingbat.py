a09ebd818a9dcaaa7ed081796cb40c995eed54d3
Elijah Robert Colwill <colwiller@dcsdk12.org>
2016-09-21 12:30:11 -0600

    Implement sum2 function

--- codingbat.py
+++ codingbat.py
@@ -249,3 +249,15 @@ print "--"
 print max_end3([1, 2, 3]) # [3, 3, 3]
 print max_end3([11, 5, 9]) # [11, 11, 11]
 print max_end3([2, 11, 3]) # [3, 3, 3]
+
+def sum2(nums):
+    if len(nums) == 0:
+        return 0
+    elif len(nums) == 1:
+        return nums[0]
+    else: return nums[0] + nums[1]
+
+print "--"
+print sum2([1, 2, 3]) # 3
+print sum2([1, 1]) # 2
+print sum2([1, 1, 1, 1]) # 2
