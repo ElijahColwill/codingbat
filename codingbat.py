e136e78f4bee76b1c4110f3cfe1641abcca63391
Elijah Robert Colwill <colwiller@dcsdk12.org>
2016-09-21 12:20:17 -0600

    Implement reverse3 function

--- codingbat.py
+++ codingbat.py
@@ -226,3 +226,11 @@ print "--"
 print rotate_left3([1, 2, 3]) # [2, 3, 1]
 print rotate_left3([5, 11, 9]) # [11, 9, 5]
 print rotate_left3([7, 0, 0]) # [0, 0, 7]
+
+def reverse3(nums):
+    return [nums[2], nums[1], nums[0]]
+
+print "--"
+print reverse3([1, 2, 3]) # [3, 2, 1]
+print reverse3([5, 11, 9]) # [9, 11, 5]
+print reverse3([7, 0, 0]) # [0, 0, 7]
