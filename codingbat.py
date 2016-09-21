aaaf05078e275378f9a6f00698b182d73529588c
Elijah Robert Colwill <colwiller@dcsdk12.org>
2016-09-21 12:40:44 -0600

    Implement count_enevs function

--- codingbat.py
+++ codingbat.py
@@ -287,3 +287,15 @@ print has23([4, 3]) # True
 print has23([4, 5]) # False
 
 # LIST 2
+
+def count_evens(nums):
+    result = 0
+    for i in nums:
+        if (i % 2) == 0:
+            result += 1
+    return result
+
+print "--"
+print count_evens([2, 1, 2, 3, 4]) # 3
+print count_evens([2, 2, 0]) # 3
+print count_evens([1, 3, 5]) # 0
