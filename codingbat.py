725b82fa7bddfb88d939cae6a3b1af438ba7d9ff
Elijah Robert Colwill <colwiller@dcsdk12.org>
2016-09-21 12:31:56 -0600

    Implement middle_way function

--- codingbat.py
+++ codingbat.py
@@ -261,3 +261,11 @@ print "--"
 print sum2([1, 2, 3]) # 3
 print sum2([1, 1]) # 2
 print sum2([1, 1, 1, 1]) # 2
+
+def middle_way(a, b):
+    return [a[1], b[1]]
+
+print "--"
+print middle_way([1, 2, 3], [4, 5, 6]) # [2, 5]
+print middle_way([7, 7, 7], [3, 8, 0]) # [7, 8]
+print middle_way([5, 2, 9], [1, 4, 5]) # [2, 4]
