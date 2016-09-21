1e0fd1c9877c5c4f56e0c121e1894c4e4e1afa27
Elijah Robert Colwill <colwiller@dcsdk12.org>
2016-09-21 11:58:51 -0600

    Implement same_first_last function

--- codingbat.py
+++ codingbat.py
@@ -180,3 +180,15 @@ print "--"
 print first_last6([1, 2, 6]) # True
 print first_last6([6, 1, 2, 3]) # True
 print first_last6([13, 6, 1, 2, 3]) # False
+
+def same_first_last(nums):
+    if len(nums) > 0:
+        if nums[0] == nums[-1]:
+            return True
+        else: return False
+    else: return False
+
+print "--"
+print same_first_last([1, 2, 3]) # False
+print same_first_last([1, 2, 3, 1]) # True
+print same_first_last([1, 2, 1]) # True
